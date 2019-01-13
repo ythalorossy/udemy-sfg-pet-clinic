2811dec86ab161f37d76255b2119fdb29d928d1c
Ythalo Rossy <ythalorossy@gmail.com>
2019-01-12 20:06:24 -0500

    Added Vet List and update controller. Closes #15

--- pet-clinic-data/src/main/java/guru/springframework/sfggetclinic/services/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfggetclinic/services/map/VetServiceMap.java
@@ -17,22 +17,22 @@ public class VetServiceMap
     }
 
     @Override
-    public Vet findById(Long id) {
-        return null;
-    }
-
-    @Override
-    public Vet save(Vet object) {
-        return null;
-    }
-
-    @Override
     public void deleteById(Long id) {
         super.deleteById(id);
     }
 
     @Override
     public void delete(Vet object) {
+        super.delete(object);
+    }
 
+    @Override
+    public Vet save(Vet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return super.findById(id);
     }
 }
