f091b6b33b4a54aa79d71dd0142aacdf17f051fe
Ythalo Rossy <ythalorossy@gmail.com>
2019-01-19 11:12:33 -0500

    Added visits, update owner with pet relashionship. Closes #26

--- pet-clinic-data/src/main/java/guru/springframework/sfggetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfggetclinic/model/Pet.java
@@ -3,6 +3,7 @@ package guru.springframework.sfggetclinic.model;
 import java.time.LocalDate;
 
 public class Pet  extends BaseEntity{
+
     private PetType petType;
     private Owner owner;
     private LocalDate birthDate;
