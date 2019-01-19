cc8dcfddec9eb74e23361ad41fd38d823558dc78
Ythalo Rossy <ythalorossy@gmail.com>
2019-01-19 11:16:08 -0500

    Added specialities, Added to Vet. Closes #27

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfggetclinic/model/Specialty.java
@@ -0,0 +1,14 @@
+package guru.springframework.sfggetclinic.model;
+
+public class Specialty extends BaseEntity {
+
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
--- pet-clinic-data/src/main/java/guru/springframework/sfggetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfggetclinic/model/Vet.java
@@ -1,4 +1,16 @@
 package guru.springframework.sfggetclinic.model;
 
+import java.util.Set;
+
 public class Vet extends Person {
+
+    private Set<Specialty> specialities;
+
+    public Set<Specialty> getSpecialities() {
+        return specialities;
+    }
+
+    public void setSpecialities(Set<Specialty> specialities) {
+        this.specialities = specialities;
+    }
 }
